7f9a08686d0ecd501f084c4afba0fe8a72b7f338
VAkarsh20 <vakarsh2000@gmail.com>
2024-09-08 03:05:08 -0500

    Beetlejuice

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
-GEMINI_REQUEST_LIMIT = 15
+GEMINI_REQUEST_LIMIT = 10
 GEMINI_PROMPT = "Proofread (focus on spelling and grammar, do not provide any explantation, do not use a semi-colon in grammar fixes, replace the end of each sentence with a ';', the last sentence should not end with any punctuation)"
 
 # Mapping rating to its respective star
@@ -13,7 +13,7 @@ stars_ratings_dict = {
     2.0: (4.0, 5.0),
     1.5: (3.0, 4.0),
     1.0: (2.0, 3.0),
-    0.5: (1.0, 2.0)
+    0.5: (1.0, 2.0),
 }
 
 # Mapping rating to its respective IMDb rating
@@ -28,7 +28,7 @@ imdb_ratings_dict = {
     4: (4.0, 5.0),
     3: (3.0, 4.0),
     2: (2.0, 3.0),
-    1: (1.0, 2.0)
+    1: (1.0, 2.0),
 }
 
 # Mapping rating to its respective tag
@@ -49,19 +49,29 @@ tags_ratings_dict = {
 
 # IMDB XPATHs
 IMDB_STARS_ELEMENTS_CLASS_NAME = "ice-star-wrapper"
-IMDB_HEADLINE_ELEMENT_XPATH = '/html/body/div[1]/div/div/div/div[1]/div[5]/div[1]/input'
-IMDB_REVIEW_ELEMENT_XPATH = '/html/body/div[1]/div/div/div/div[1]/div[5]/div[2]/textarea'
-IMDB_NO_SPOLIERS_ELEMENT_XPATH = '/html/body/div[1]/div/div/div/div[1]/div[5]/div[3]/div/ul/li[2]'
-IMDB_SUBMIT_ELEMENT_XPATH = '/html/body/div[1]/div/div/div/div[2]/span/span/input'
+IMDB_HEADLINE_ELEMENT_XPATH = "/html/body/div[1]/div/div/div/div[1]/div[5]/div[1]/input"
+IMDB_REVIEW_ELEMENT_XPATH = (
+    "/html/body/div[1]/div/div/div/div[1]/div[5]/div[2]/textarea"
+)
+IMDB_NO_SPOLIERS_ELEMENT_XPATH = (
+    "/html/body/div[1]/div/div/div/div[1]/div[5]/div[3]/div/ul/li[2]"
+)
+IMDB_SUBMIT_ELEMENT_XPATH = "/html/body/div[1]/div/div/div/div[2]/span/span/input"
 IMDB_ADD_TO_LIST_SEARCH_ELEMENT_XPATH = '//*[@id="add-to-list-search"]'
-IMDB_FIRST_MOVIE_IN_SEARCH_ELEMENT_XPATH = "/html/body/div[2]/div/div[2]/div[3]/div[1]/div[2]/div[5]/div/span[2]/div/a"
-IMDB_SAVE_LIST_ELEMENT_XPATH = '/html/body/div[2]/div/div[2]/div[3]/div[1]/div[1]/button'
+IMDB_FIRST_MOVIE_IN_SEARCH_ELEMENT_XPATH = (
+    "/html/body/div[2]/div/div[2]/div[3]/div[1]/div[2]/div[5]/div/span[2]/div/a"
+)
+IMDB_SAVE_LIST_ELEMENT_XPATH = (
+    "/html/body/div[2]/div/div[2]/div[3]/div[1]/div[1]/button"
+)
 
 # IMDB URLs
 IMDB_HOME_PAGE_URL = "https://www.imdb.com/"
 IMDB_MOVIE_PAGE_URL = "https://www.imdb.com/title/{}"
 IMDB_ADD_MOVIE_REVIEW_PAGE_URL = "https://contribute.imdb.com/review/{}/add?"
-IMDB_CINEMA_PERSONIFIED_LIST_URL = "https://www.imdb.com/list/ls520163773/edit?ref_=ttls_edt"
+IMDB_CINEMA_PERSONIFIED_LIST_URL = (
+    "https://www.imdb.com/list/ls520163773/edit?ref_=ttls_edt"
+)
 
 # Letterboxd File Strings
 LETTERBOXD_UPLOAD_CSV = "letterboxd_upload.csv"
@@ -83,9 +93,15 @@ LETTERBOXD_SIGN_IN_ELEMENT_XPATH = "/html/body/div[1]/div/form/div/div[3]/button
 LETTERBOXD_UPLOAD_IMDB_IMPORT_ELEMENT_ID = "upload-imdb-import"
 LETTERBOXD_SUBMIT_REVIEW_ELEMENT_XPATH = '//*[@id="content"]/div/div[1]/a[2]'
 LETTERBOXD_MOVIE_IMAGE_ELEMENT = "link text"
-LETTERBOXD_LIKE_TEXT_ELEMENT_XPATH = '//*[@id="userpanel"]/ul/li[1]/span[2]/span/span/span'
-LETTERBOXD_LIKE_BUTTON_ELEMENT_XPATH = "/html/body/div[2]/div/div/aside/section[1]/ul/li[1]/span[2]/span/span/span"
-LETTERBOXD_CINEMA_PERSONIFIED_LIST_ELEMENT_XPATH = '/html/body/div[6]/div[1]/div[2]/div[2]/div[1]/div/form/div[2]/div[1]/label'
-LETTERBOXD_ADD_TO_LIST_ELEMENT_XPATH = '//*[@id="add-to-a-list-modal"]/form/div[3]/div[2]/input'
-
-
+LETTERBOXD_LIKE_TEXT_ELEMENT_XPATH = (
+    '//*[@id="userpanel"]/ul/li[1]/span[2]/span/span/span'
+)
+LETTERBOXD_LIKE_BUTTON_ELEMENT_XPATH = (
+    "/html/body/div[2]/div/div/aside/section[1]/ul/li[1]/span[2]/span/span/span"
+)
+LETTERBOXD_CINEMA_PERSONIFIED_LIST_ELEMENT_XPATH = (
+    "/html/body/div[6]/div[1]/div[2]/div[2]/div[1]/div/form/div[2]/div[1]/label"
+)
+LETTERBOXD_ADD_TO_LIST_ELEMENT_XPATH = (
+    '//*[@id="add-to-a-list-modal"]/form/div[3]/div[2]/input'
+)
--- utils/proofread_utils.py
+++ utils/proofread_utils.py
@@ -3,16 +3,18 @@ import time
 import google.generativeai as genai
 from utils.constants import GEMINI_REQUEST_LIMIT, GEMINI_PROMPT
 
+
 def proofread_comments(comments, model):
     parts = comments.split(";")
     try:
         result = model.generate_content("{}: {}".format(GEMINI_PROMPT, str(parts)))
-        return result.text.replace("**","").rstrip()
+        return result.text.replace("**", "").rstrip()
     except ValueError as e:
-        print("Cannot produce a result for comments: \"{}\"".format(comments))
+        print('Cannot produce a result for comments: "{}"'.format(comments))
         return comments
 
-def wait(counter, request_batch, sleep_time = 65):
+
+def wait(counter, request_batch, sleep_time=65):
     if counter / request_batch > GEMINI_REQUEST_LIMIT:
         print("Above Google Gemini request limit, waiting 1 minute before next batch")
         time.sleep(sleep_time)
@@ -20,12 +22,13 @@ def wait(counter, request_batch, sleep_time = 65):
     else:
         return False
 
+
 def create_model():
     # Load yaml file
-    yml = yaml.safe_load(open('login_details.yml'))
+    yml = yaml.safe_load(open("login_details.yml"))
 
     # Configure API key (replace with your actual key)
-    genai.configure(api_key=yml['gemini']['api_key'])
+    genai.configure(api_key=yml["gemini"]["api_key"])
 
     # Generate text using the prompt
     return genai.GenerativeModel("gemini-1.5-flash")
